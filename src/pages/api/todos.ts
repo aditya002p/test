49ee163683a096a4d16263c428b5416cf94f09c3
Aditya <ricky.pathak902@gmail.com>
2024-12-21 22:51:12 +0530

    helo

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -7,7 +7,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default async function handler(
   req: NextApiRequest,
